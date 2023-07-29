eb52dd4dd90cef0ed4ae944261e800b527b65c1d
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-07-29 23:03:17 +0800

    Attempts to add database

--- src/renderer/pages/ToDoList.jsx
+++ src/renderer/pages/ToDoList.jsx
@@ -1,5 +1,32 @@
+/* eslint-disable consistent-return */
+const sqlite3 = require('sqlite3').verbose();
+
 function ToDoList() {
-  return <h1>This is TODO list</h1>;
+  const data = [];
+  const db = new sqlite3.Database('./public/radar_db.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('connected to in-memory sqlite database');
+  });
+
+  db.serialize(() => {
+    db.each('SELECT * FROM TASKS', (err, row) => {
+      if (err) {
+        console.log(err.message);
+      }
+      data.push(row.Name);
+      console.log(row.ID + '\t' + row.Name);
+    });
+  });
+
+  db.close();
+  return (
+    <>
+      <h1>TODO LIST</h1>
+      <p>{data}</p>
+    </>
+  );
 }
 
 export default ToDoList;
